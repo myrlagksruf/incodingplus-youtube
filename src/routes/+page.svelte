385a6ca0d3307582f2d47769dfe1453fa35a0fbc
myrlagksruf <myrlagksruf@gmail.com>
2023-02-24 19:10:46 +0900

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import YouTubePlayer from 'youtube-player';
+    onMount(async () => {
+        let firstStateChange;
+
+        let player1 = YouTubePlayer('player-1', {
+            playerVars:{
+            }
+        });
+        await player1.playVideo()
+        // await player1.loadPlaylist('PLMoZgWjm14OF2AWFfPc3cyQH7T2SC89jo');
+    })
+</script>
+
+<div id='player-1'></div>
